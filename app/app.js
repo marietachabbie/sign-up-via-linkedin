afe8bfd1de9ed13ed335ae965304322ffed6fb21
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-11-13 00:08:40 +0400

    fully functional working code without auth credentials (forr safety reasons: temporary), rm mongoDB connection

--- app/app.js
+++ app/app.js
@@ -4,9 +4,19 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const livereload = require("livereload");
 const connectLiveReload = require("connect-livereload");
-const MongoDbConnection = require('./models/MongoDbConnection');
-
 const indexRouter = require('./routes');
+
+const passport = require("passport");
+const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
+const session = require('express-session')
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 const liveReloadServer = livereload.createServer();
 liveReloadServer.server.once("connection", () => {
@@ -27,14 +37,46 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: "SESSION_SECRET" }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
 
-const main = () => {
-  MongoDbConnection.init()
-  .then(() => app.use('/', indexRouter))
-  .catch (error => output(error));
-}
+const LINKEDIN_KEY = '';
+const LINKEDIN_SECRET = '';
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: LINKEDIN_KEY,
+      clientSecret: LINKEDIN_SECRET,
+      callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+      scope: ["r_emailaddress", "r_liteprofile"],
+    },
+    (
+      accessToken,
+      refreshToken,
+      profile,
+      done
+    ) => {
+      process.nextTick(() => {
+        return done(null, profile);
+      });
+    }
+  )
+);
 
-main()
+app.get(
+  "/auth/linkedin",
+  passport.authenticate("linkedin", { state: "" })
+);
+
+app.get(
+  "/auth/linkedin/callback",
+  passport.authenticate("linkedin", {
+    successRedirect: "/sign-in-via-ln",
+    failureRedirect: "/",
+  })
+);
 
 // error handler
 app.use(function(err, req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('homepage');
+  res.render('homepage')
+});
+
+router.get('/sign-in-via-ln', function(req, res, next) {
+  const name = req.user.name.givenName;
+  const family = req.user.name.familyName;
+  const photo = req.user.photos[1].value;
+  const email = req.user.emails[0].value;
+
+  const data = { name, family, photo, email }
+  res.render('auth-user-page', { data })
 });
 
 module.exports = router;
